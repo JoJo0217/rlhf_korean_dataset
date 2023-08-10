bae00be704384dddc83d4a7fcc590a5c3faae7fb
JoJo0217 <mingu0217@gmail.com>
2023-08-10 13:35:24 +0900

    added llm team data, make step3 no hatespeech data

--- filter_same_and_share.py
+++ filter_same_and_share.py
@@ -2,15 +2,15 @@ import json
 import jsonlines
 
 # 첫 번째 jsonl 파일을 읽습니다.
-with open("./step2/no_hate.jsonl", "r") as file1:
+with open("./step3/raw_data/step3_step1_data.jsonl", "r") as file1:
     data1 = [json.loads(line) for line in file1.readlines()]
 
 # 두 번째 jsonl 파일을 읽습니다.
-with open("./whole_gamseong.jsonl", "r") as file2:
+with open("./step1/llm_team/hatespeech.jsonl", "r") as file2:
     data2 = [json.loads(line) for line in file2.readlines()]
 
 # 두 번째 파일에 있는 prompts를 추출합니다.
-prompts_in_file2 = {item["prompt"] for item in data2}
+prompts_in_file2 = {item["instruction"] for item in data2}
 
 # 첫 번째 파일에서 두 번째 파일에 없는 prompt를 가진 데이터만 필터링합니다.
 filtered_data = [item for item in data1 if item["prompt"] not in prompts_in_file2]
